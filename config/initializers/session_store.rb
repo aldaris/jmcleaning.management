66ddc814ad4c73f3d3504d7716b85c4a6c717bab
Peter Major <peter.major@forgerock.com>
2018-08-05 17:10:36 +0100

    Enable same-site and httponly

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_session', same_site: :lax
+Rails.application.config.session_store :cookie_store, key: '_session', same_site: :lax, http_only: true
