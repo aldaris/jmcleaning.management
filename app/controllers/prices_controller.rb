a2fc108a0add472d5e1ba7ce45b8980a5ed2c5d8
Peter Major <majorpetya@gmail.com>
2018-08-28 21:52:24 +0100

    Price pages

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,6 +1,23 @@
 class PricesController < ApplicationController
 
+  def index
+    prices = Price.all.order(:amount)
+    @active_prices = prices.select {|price| price.is_active}
+    @inactive_prices = prices.select {|price| !price.is_active}
+  end
+
   def new
     @price = Price.new
+  end
+
+  def create
+    Price.create(price_params)
+    redirect_to prices_url
+  end
+
+  private
+
+  def price_params
+    params.require(:price).permit(:description, :amount, :is_active)
   end
 end
