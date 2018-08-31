050c489d2ad83d879d0feb996e6901de20b2078b
Peter Major <majorpetya@gmail.com>
2018-08-31 22:36:17 +0100

    Display price as well in the dropdown

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,4 +1,8 @@
 class Price < ApplicationRecord
   has_many :invoice_items
   validates :description, :amount, presence: true
+
+  def description_with_price
+    "#{description} - £#{'%.2f' % amount}"
+  end
 end
