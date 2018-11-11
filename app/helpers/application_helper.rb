773142198aa9dafcdbea0dc8233ac36f2e8f491d
Peter Major <majorpetya@gmail.com>
2018-11-11 10:17:56 +0000

    Improve nav highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def active_class(link_path)
-    current_page?(link_path) ? 'nav-item nav-link active' : 'nav-item nav-link'
+    request.path.start_with?(link_path) ? 'nav-item nav-link active' : 'nav-item nav-link'
   end
 end
