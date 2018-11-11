dc24fa290e761a8af112118ff683d050ca525ed3
Peter Major <majorpetya@gmail.com>
2018-11-11 10:18:21 +0000

    Remove phone number field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_125158) do
+ActiveRecord::Schema.define(version: 2018_11_11_101019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2018_08_02_125158) do
     t.string "name"
     t.string "nick_name"
     t.string "email_address"
-    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
