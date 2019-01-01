eee59443e30dd76efda4faed953544e54c10d141
Peter Major <majorpetya@gmail.com>
2019-01-01 16:22:22 +0000

    Fix active nav item detection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def active_class(link_path)
-    request.path.start_with?(link_path) ? 'nav-item nav-link active' : 'nav-item nav-link'
+    if (request.path == '/' && link_path == invoices_path) || request.path.start_with?(link_path)
+      'nav-item nav-link active'
+    else
+      'nav-item nav-link'
+    end
   end
 end
